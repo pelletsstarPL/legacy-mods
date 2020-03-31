72db7b138d3f1771acce8156975a00d306e818d3
Jake <JakeSESaunders@outlook.com>
2020-03-31 17:23:05 +0100

    Changed iconsheet filename

--- mod.js
+++ mod.js
@@ -5,7 +5,7 @@ G.AddData({
     engineVersion:1,
     manifest:'modManifest.js',
     requires:['Default dataset*'],
-    sheets:{'spicySheet':'img/spicyModIconSheet.png'},//custom stylesheet (note : broken in IE and Edge for the time being)
+    sheets:{'spicySheet':'img/pepperIconSheet.png'},//custom stylesheet (note : broken in IE and Edge for the time being)
     func:function()
     {
         //The idea in this simple example mod is to add a few elements focused around hot sauce, because hot sauce is great and I use that stuff everywhere.
