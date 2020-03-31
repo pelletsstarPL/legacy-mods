000992f83da9268193f6ff2211ba4da1a1ec04f1
Jake <JakeSESaunders@outlook.com>
2020-03-31 17:08:27 +0100

    Experiment to see how representing icons in descs works

--- jake.js
+++ jake.js
@@ -15,7 +15,7 @@ G.AddData({
         //First we create a couple new resources :
         new G.Res({
             name:'hot pepper',
-            desc:'[hot pepper]s are loaded with capsaicin and, depending on who you ask, may produce a pleasant burn when eaten.',
+            desc:'[hot pepper]s are loaded with capsaicin and [herb, herby]s, depending on who you ask, may produce a pleasant burn when eaten.',
             icon:[0,0,'spicySheet'],
             turnToByContext:{'eat':{'health':0.01,'happiness':0.03},'decay':{'spoiled food':0.5}},//this basically translates to : "when eaten, generate some health and happiness; when rotting, turn into either nothing or some spoiled food"
             partOf:'food',
