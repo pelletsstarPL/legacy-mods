c04640c875507a94910ca89e8af47e262b43cb0c
Jake <JakeSESaunders@outlook.com>
2020-04-03 14:00:45 +0100

    Fully changed from hot peppers to purple peppers

--- jake.js
+++ jake.js
@@ -3,7 +3,7 @@ G.AddData({
     author:'Orteil (Jake)',
     desc:'A simple mod that adds hot peppers and hot sauce.',
     engineVersion:1,
-    manifest:'ModManifest.js',
+    manifest:'',
     requires:['Default dataset*'],
     sheets:{'spicySheet':'https://raw.githubusercontent.com/JakeSESaunders/Legacy-Mod/master/img/pepperIconSheet.png'},//custom stylesheet (note : broken in IE and Edge for the time being)
     func:function()
@@ -14,16 +14,16 @@ G.AddData({
         
         //First we create a couple new resources :
         new G.Res({
-            name:'hot pepper',
-            desc:'[hot pepper]s are loaded with capsaicin and [herb, herby]s, depending on who you ask, may produce a pleasant burn when eaten.',
+            name:'purple pepper',
+            desc:'[purple pepper]s are loaded with purple, depending on who you ask, may produce a pleasant purple when eaten.',
             icon:[0,0,'spicySheet'],
             turnToByContext:{'eat':{'health':0.01,'happiness':0.03},'decay':{'spoiled food':0.5}},//this basically translates to : "when eaten, generate some health and happiness; when rotting, turn into either nothing or some spoiled food"
             partOf:'food',
             category:'food',
         });
         new G.Res({
-            name:'hot sauce',
-            desc:'Made from [herb]s and the [hot pepper,Spiciest peppers], this [hot sauce] stays fresh for a while and will leave anyone panting and asking for more.',
+            name:'purple sauce',
+            desc:'Made from [herb]s and the [purple pepper, purple-st peppers], this [purple sauce] stays fresh for a while and will leave anyone panting and asking for more.',
             icon:[1,0,'spicySheet'],
             turnToByContext:{'eat':{'health':0.03,'happiness':0.1},'decay':{'hot sauce':0.95,'spoiled food':0.05}},//that last part makes hot sauce effectively have a 95% chance of simply not rotting (in effect, it decays into itself)
             partOf:'food',
@@ -32,16 +32,16 @@ G.AddData({
         
         //Then we augment the base data to incorporate our new resources :
             //adding hot pepper as something that can be gathered from grass
-        G.getDict('grass').res['gather']['hot pepper']=3;
+        G.getDict('grass').res['gather']['purple pepper']=3;
             //adding a new mode to artisans so they can make hot sauce from hot peppers
-        G.getDict('artisan').modes['hot sauce']={name:'Make hot sauce',desc:'Turn 3 [hot pepper]s and 3 [herb]s into 1 [hot sauce].',req:{'hot sauce preparing':true},use:{'knapped tools':1}};
+        G.getDict('artisan').modes['purple sauce']={name:'Make purple sauce',desc:'Turn 3 [purple pepper]s and 3 [herb]s into 1 [purple sauce].',req:{'purple sauce preparing':true},use:{'knapped tools':1}};
             //adding a new effect to artisans that handles the actual hot sauce preparing and is only active when the unit has the mode "hot sauce"
-        G.getDict('artisan').effects.push({type:'convert',from:{'hot pepper':3,'herb':3},into:{'hot sauce':1},every:3,mode:'hot sauce'});
+        G.getDict('artisan').effects.push({type:'convert',from:{'purple pepper':3,'herb':3},into:{'purple sauce':1},every:3,mode:'purple sauce'});
         
         //Then we add a new technology which is required by the artisans to gain access to the "hot sauce" mode :
         new G.Tech({
-            name:'hot sauce preparing',
-            desc:'@[artisan]s can now produce [hot sauce] from [hot pepper]s and [herb]s//This special recipe allows a skilled craftsman to fully express the complex aromas present in hot peppers.',
+            name:'purple sauce preparing',
+            desc:'@[artisan]s can now produce [purple sauce] from [purple pepper]s and [herb]s//This special recipe allows a skilled craftsman to fully express the complex aromas present in hot peppers.',
             icon:[0,1,'spicySheet'],
             cost:{'insight':10},
             req:{'cooking':true},
@@ -49,13 +49,13 @@ G.AddData({
         
         //Finally, we add a trait that amplifies the benefits of consuming hot sauce; it will take on average 20 years to appear once the conditions (knowing the "Hot sauce preparing" tech) is fulfilled.
         new G.Trait({
-            name:'hot sauce madness',
-            desc:'@your people appreciate [hot sauce] twice as much and will be twice as happy from consuming it.',
+            name:'purple sauce madness',
+            desc:'@your people appreciate [purple sauce] twice as much and will be twice as happy from consuming it.',
             icon:[1,1,'spicySheet'],
             chance:20,
-            req:{'hot sauce preparing':true},
+            req:{'purple sauce preparing':true},
             effects:[
-                {type:'function',func:function(){G.getDict('hot sauce').turnToByContext['eat']['happiness']=0.2;}},//this is a custom function executed when we gain the trait
+                {type:'function',func:function(){G.getDict('purple sauce').turnToByContext['eat']['happiness']=0.2;}},//this is a custom function executed when we gain the trait
             ],
         });
         
